7d01b3f4df87e47fdff9110fc28f4b643da6429b
inzamulhaque <mdihalif@gmail.com>
2022-05-01 01:07:30 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="bg-white dark:bg-black min-h-screen">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <footer className={`bg-black absolute w-full bottom-0 py-2 md:py-3 lg:py-5`}>
+            <p className="text-center text-[18px] text-white">
+                copyright &copy; {year} || <a href="https://web.facebook.com/mdihalif.2002/" className='text-center text-[18px] text-white' target={"_blank"}>MD IH Alif</a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
