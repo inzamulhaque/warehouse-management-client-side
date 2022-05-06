959c25c3845aaccfa87646221a3e238a75998d5e
inzamulhaque <mdihalif@gmail.com>
2022-05-07 01:49:32 +0600

    node js vs mongodb Q&A

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import JsVsNodeJS from './JsVsNodeJS';
+import NodeVsMongo0db from './NodeVsMongo0db';
 
 const Blogs = () => {
     return (
@@ -8,6 +9,9 @@ const Blogs = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div className="my-2 py-2 px-3 card dark:bg-white h-100 mx-auto w-full">
                         <JsVsNodeJS />
+                    </div>
+                    <div className="my-2 py-2 px-3 card dark:bg-white h-100 mx-auto w-full">
+                        <NodeVsMongo0db />
                     </div>
                 </div>
             </div>
---
+++ src/components/Blogs/NodeVsMongo0db.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NodeVsMongo0db = () => {
+    return (
+        <>
+            <h3 className="text-[25px] font-semibold dark:text-blue-400 m-2">When should you use nodejs and when should you use mongodb</h3>
+            <div className="m-2">
+                <p className="text-[20px] font-medium text-justify">
+                    NodeJS JavaScript এর Run-Time Environment। এটি আমাদের browser এর বাহিরে JavaScript Run করার ব্যবস্থা করে দেয়।
+                </p>
+                <p className="text-[20px] font-medium text-justify">
+                    MongoDB একটি Database management system। এটি আমাদের data গুলো store এবং সেগুলো management করতে দেয়।
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default NodeVsMongo0db;
